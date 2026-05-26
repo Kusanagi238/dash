39163f42c3cc2bbe9c830b583bf92d8c75277e1b
Adrian Borrmann <hello@adrianborrmann.com>
2026-05-26 11:30:33 -0600

    Implement configuration function to customize MCP server

--- dash/dash.py
+++ dash/dash.py
@@ -488,7 +488,6 @@ class Dash(ObsoleteChecker):
         websocket_inactivity_timeout: Optional[int] = 300000,
         enable_mcp: Optional[bool] = None,
         mcp_path: Optional[str] = None,
-        mcp_expose_docstrings: Optional[bool] = None,
         **obsolete,
     ):
 
@@ -570,9 +569,6 @@ class Dash(ObsoleteChecker):
             hide_all_callbacks=False,
             csrf_token_name=csrf_token_name,
             csrf_header_name=csrf_header_name,
-            mcp_expose_docstrings=get_combined_config(
-                "mcp_expose_docstrings", mcp_expose_docstrings, False
-            ),
         )
         self.config.set_read_only(
             [
@@ -616,6 +612,7 @@ class Dash(ObsoleteChecker):
         self._callback_list: list = []
         self.callback_api_paths: dict = {}
         self.mcp_decorated_functions: dict = {}
+        self.mcp_callback_map: Any = None
 
         # list of inline scripts
         self._inline_scripts: list = []
